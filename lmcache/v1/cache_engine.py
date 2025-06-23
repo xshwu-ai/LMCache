b387fe2ad52703900857985c4a564d4b14c6c8b2
xshwu <xshwu@tencent.com>
2025-06-23 15:11:08 +0800

    fix: log panic when memory_obj is None

--- lmcache/v1/cache_engine.py
+++ lmcache/v1/cache_engine.py
@@ -284,7 +284,6 @@ class LMCacheEngine:
             #memory_obj = self.storage_manager.get(key)
             if tg.is_first_rank:
                 memory_obj = self.storage_manager.get(key)
-                logger.info(f"retrive storage_manager.get {memory_obj.metadata.to_dict()=} {tg.rank=}, cost: {round((time.time()-t)*1000)}")
 
                 if memory_obj is None:
                     if self.enable_p2p:
@@ -296,6 +295,7 @@ class LMCacheEngine:
                     if memory_obj is None:
                         break
                 # broadcast
+                logger.info(f"retrive storage_manager.get {memory_obj.metadata.to_dict()=} {tg.rank=}, cost: {round((time.time()-t)*1000)}")
                 tg.broadcast_object(memory_obj.metadata.to_dict(), tg.first_rank)
                 logger.info(f"retrive broadcast_object metadata {tg.rank=}, cost: {round((time.time()-t)*1000)}")
                 tg.broadcast(memory_obj.tensor.to(f"cuda:{tg.rank}"), tg.first_rank)
