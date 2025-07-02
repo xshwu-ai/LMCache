091497679db1853ddb8f75166f5f57f097326340
xshwu <xshwu@tencent.com>
2025-07-02 17:33:30 +0800

    feat: support caching switch

--- lmcache/integration/vllm/vllm_v1_adapter.py
+++ lmcache/integration/vllm/vllm_v1_adapter.py
@@ -191,6 +191,8 @@ class RequestTracker:
     mm_positions: Optional[list["PlaceholderRange"]] = None
     # The request user
     user: Optional[str] = None
+    # The request caching flag
+    caching: Optional[bool] = False
 
     # Whether the request is in decode phase
     is_decode_phase: bool = False
@@ -237,6 +239,7 @@ class RequestTracker:
             mm_hashes=new_request.mm_hashes.copy(),
             mm_positions=new_request.mm_positions.copy(),
             user=new_request.user,
+            caching=new_request.caching,
         )
 
     def update(
@@ -276,7 +279,9 @@ class ReqMeta:
     # load_spec
     load_spec: Optional[LoadSpec] = None
     # user
-    user: Optional[str] = None
+    user: Optional[str] = ""
+    # caching
+    caching: Optional[bool] = False
 
     @staticmethod
     def from_request_tracker(
@@ -392,6 +397,7 @@ class ReqMeta:
             save_spec=save_spec,
             load_spec=load_spec,
             user=tracker.user,
+            caching=tracker.caching,
         )
 
 
@@ -544,6 +550,9 @@ class LMCacheConnectorV1Impl:
 
         self.layerwise_retrievers = []
         for idx, request in enumerate(metadata.requests):
+            logger.info(f"start_load_kv: {request.req_id=}, {request.caching=}")
+            if not request.caching:
+                continue
             if request.load_spec is None:
                 continue
 
@@ -669,6 +678,9 @@ class LMCacheConnectorV1Impl:
             self.layerwise_storers = []
 
             for idx, request in enumerate(connector_metadata.requests):
+                logger.info(f"save_kv_layer: {request.req_id=}, {request.caching=}")
+                if not request.caching:
+                    continue
                 save_spec = request.save_spec
                 if save_spec is None or not save_spec.can_save:
                     continue
@@ -749,6 +761,9 @@ class LMCacheConnectorV1Impl:
         assert self.lmcache_engine is not None
 
         for request in connector_metadata.requests:
+            logger.info(f"wait_for_save: {request.req_id=}, {request.caching=}")
+            if not request.caching:
+                continue
             save_spec = request.save_spec
             if save_spec is None or not save_spec.can_save:
                 continue
@@ -834,6 +849,9 @@ class LMCacheConnectorV1Impl:
         if self.kv_role == "kv_producer" and not hasattr(
             self.lookup_client, "supports_producer_reuse"
         ):
+            return 0
+        
+        if not request.caching:
             return 0
 
         token_ids = torch.tensor(request.prompt_token_ids)
