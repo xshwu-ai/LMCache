ef94d218b8d95cb08b7ba5a4106512782c4e92dc
rigginschen <rigginschen@tencent.com>
2025-07-04 17:06:32 +0800

    fix bug for save_decode_cache
    Signed-off-by: rigginschen <rigginschen@tencent.com>

--- lmcache/integration/vllm/vllm_v1_adapter.py
+++ lmcache/integration/vllm/vllm_v1_adapter.py
@@ -90,6 +90,9 @@ class RequestTracker:
     mm_hashes: Optional[list[str]] = None
     mm_positions: Optional[list["PlaceholderRange"]] = None
 
+    # Whether the request is in decode phase
+    is_decode_phase: bool = False
+
     @staticmethod
     def from_new_request(
         new_request: "NewRequestData",
@@ -154,6 +157,8 @@ class RequestTracker:
             new_block_ids = new_block_ids[0]
         self.allocated_block_ids.extend(new_block_ids)
 
+        if len(cached_request.new_token_ids) == 1:
+            self.is_decode_phase = True
 
 @dataclass
 class ReqMeta:
@@ -209,8 +214,8 @@ class ReqMeta:
             cdiv(tracker.num_saved_tokens + 1, lmcache_chunk_size) * lmcache_chunk_size
         )
         skip_save = skip_save or (
-            tracker.num_saved_tokens > 0 and not save_decode_cache
-        )
+            tracker.num_saved_tokens > 0 and input_token_len < chunk_boundary
+        ) or (not save_decode_cache and tracker.is_decode_phase)
 
         if skip_save and load_spec is None:
             return None
